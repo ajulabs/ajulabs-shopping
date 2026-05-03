6c33cc905307ed2c62ec6223fc40afeaefec427e
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2026-05-03 16:13:59 -0300

    refactor(lojista):adicionar estrutura de tela logística

---
+++ app/(lojista)/logistica.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Logistica() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FAFBFE' }}>
+      <Text>Logística</Text>
+    </View>
+  );
+}
